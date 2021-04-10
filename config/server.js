00a48f5dd8b92596bd69fc25125770a62a818989
Marco Atencio <Marco_Atencio@hotmail.com>
2021-04-10 15:17:25 -0500

    fix tables

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 7337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '80de50728538bcbd165ec78b46bd0ea9'),
+      secret: env("ADMIN_JWT_SECRET", "80de50728538bcbd165ec78b46bd0ea9"),
     },
   },
 });
