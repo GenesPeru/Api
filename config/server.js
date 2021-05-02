05367de3b43faa135a5d899e0f6aa09c81d6c825
Marco Atencio <Marco_Atencio@hotmail.com>
2021-05-02 15:30:11 -0500

    add tables blog

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 7337),
+  port: env.int("PORT", 3306),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "80de50728538bcbd165ec78b46bd0ea9"),
