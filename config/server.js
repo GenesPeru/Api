5ff6e500c0d427f9a21bcc2bc6be3df04247cf7c
Marco Atencio <Marco_Atencio@hotmail.com>
2021-07-10 22:21:14 -0500

    add university

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 3306),
+  port: env.int("PORT", 3305),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "80de50728538bcbd165ec78b46bd0ea9"),
